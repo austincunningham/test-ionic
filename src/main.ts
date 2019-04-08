e095f5d2a0d291afb599042ec5f7f020cf173494
Austin Cunningham <aucunnin@redhat.com>
2019-04-08 14:20:56 +0100

    update

--- src/main.ts
+++ src/main.ts
@@ -3,28 +3,44 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import {Platform} from '@ionic/angular';
 
-//==========================add the following lines of code=====================
-import { init } from "@aerogear/app";
-import { Auth } from "@aerogear/auth";
+// ==========================add the following lines of code=====================
+import { init, AeroGearApp } from '@aerogear/app';
+import { Auth } from '@aerogear/auth';
+const SecurityService = require('@aerogear/security');
+let platform: Platform;
 
-const appConfig = require("./mobile-services.json");
-init(appConfig);
+const appConfig = require('./mobile-services.json');
+const app: AeroGearApp = init(appConfig);
+console.log('app: ', app.config);
+const appSecurity = new SecurityService.AppSecurity(app.config);
+// const authService = new Auth();
 
-const authService = new Auth();
-//let initOptions = { onLoad: "login-required" };
-
-authService.init({ onLoad: "login-required" })
-.then(() => {
-    // successful init & authentication
-    console.log('keycloak init');
+appSecurity.clientInit()
+.then(clientInit => {
+  console.log('INIT DATA :', clientInit);
+  if (clientInit.data.disabled) {
+    alert('Security Warning :' + clientInit.data.disabledMessage);
+    navigator['app'].exitApp();
+  }
 })
-.catch((err) => {
-    // initialization error
-    console.log('keycloak init failure', err);
-});
+.catch(error => error);
 
-//========================end of code block======================================
+
+// let initOptions = { onLoad: "login-required" };
+
+// authService.init({ onLoad: "login-required" })
+// .then(() => {
+//     // successful init & authentication
+//     console.log('keycloak init');
+// })
+// .catch((err) => {
+//     // initialization error
+//     console.log('keycloak init failure', err);
+// });
+
+// ========================end of code block======================================
 if (environment.production) {
   enableProdMode();
 }
